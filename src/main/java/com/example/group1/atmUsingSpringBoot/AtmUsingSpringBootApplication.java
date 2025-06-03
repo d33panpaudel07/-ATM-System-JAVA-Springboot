8099919d3fcffb2c7cf4f7c0e757d654c7ed0392
d33panpaudel07 <d33panpaudel07@gmail.com>
2025-06-03 10:28:03 +0545

    first commit

---
+++ src/main/java/com/example/group1/atmUsingSpringBoot/AtmUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.group1.atmUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmUsingSpringBootApplication.class, args);
+	}
+
+}
